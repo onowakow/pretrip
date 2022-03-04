a93cd5f096dcf738ae8dfcd5c1e1fc9d2ac849e3
onowakow <nowakowskiowen08@gmail.com>
2022-03-04 10:27:58 -0700

    Working on custom checkboxes handles by js

--- public/javascripts/sectionJS/sectionHandlers.js
+++ public/javascripts/sectionJS/sectionHandlers.js
@@ -1,5 +1,34 @@
-const attribute = document.querySelector('.attribute');
+const localStorage = window.localStorage;
 
-attribute.addEventListener('click', (e) => {
-  console.log(e);
-});
+const toggleableNodeArr = document.querySelectorAll('.toggleable');
+
+const handleClick = (e) => {
+  const id = e.target.id;
+  const index = e.target.name;
+  const json = localStorage.getItem(id);
+  let obj = {};
+
+  if (json) {
+    obj = JSON.parse(json);
+    obj[index] = true;
+  } else {
+    obj[index] = true;
+  }
+
+  const newJson = JSON.stringify(obj);
+
+  localStorage.setItem(id, newJson);
+};
+
+if (toggleableNodeArr) {
+  toggleableNodeArr.forEach((element) => {
+    element.addEventListener('click', (e) => handleClick(e));
+  });
+}
+
+const resetLocalStorageButton = document.getElementById('resetLocalStorage');
+if (resetLocalStorageButton) {
+  resetLocalStorageButton.addEventListener('click', () => {
+    localStorage.clear();
+  });
+}
