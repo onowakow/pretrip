f033b9771f1fe91730b535a61f368bcedaf44b1c
onowakow <nowakowskiowen08@gmail.com>
2022-03-08 16:04:21 -0700

    Still adding to pretrip information

--- app_api/models/staticLocalDb.js
+++ app_api/models/staticLocalDb.js
@@ -5,16 +5,16 @@ const DRIVE = 'driveshaft is intact and in place';
 const EXH = 'exhaust system is intact and in place';
 const busBodyObj = {
   title: 'body',
-  attributes: ['no damage']
-}
+  attributes: ['no damage'],
+};
 const busSideGlassObj = {
   title: 'glass',
-  attributes: ['no damage']
-},
+  attributes: ['no damage'],
+};
 const underTheBusObj = {
   title: 'under the bus',
   attributes: [LEAK, DRIVE, EXH],
-},
+};
 const U = 'unobstructed';
 const L = 'not leaking';
 const Blt = 'belt driven';
@@ -71,7 +71,7 @@ const array = [
             attributes: [SI],
           },
           {
-            title: 'air conditioner compressor I',
+            title: 'air conditioner compressor i',
             attributes: [SI, Blt, BP],
           },
           {
@@ -79,7 +79,7 @@ const array = [
             attributes: [SI, Blt, BP],
           },
           {
-            title: 'air conditioner compressor II',
+            title: 'air conditioner compressor ii',
             attributes: [SI, Blt, BP],
           },
           {
@@ -226,19 +226,19 @@ const array = [
       {
         title: 'exterior driver cab',
         components: [
-          { 
+          {
             title: 'DEF tank and cap',
-            attributes: [SI, L] 
+            attributes: [SI, L],
           },
           {
             title: 'Mirror',
-            attributes: [SI, 'not excessively loose']
+            attributes: [SI, 'not excessively loose'],
           },
           {
             title: 'steps',
-            attributes: [SI]
+            attributes: [SI],
           },
-        ]
+        ],
       },
       {
         title: 'driver side',
@@ -248,15 +248,15 @@ const array = [
           busBodyObj,
           {
             title: 'wheels',
-            attributes: ['two tires', 'check bud space',]
+            attributes: ['two tires', 'check bud space'],
           },
           {
             title: 'air tanks and tank drains',
-            attributes: []
-          }
-        ]
+            attributes: [],
+          },
+        ],
       },
-      { 
+      {
         title: 'backside',
         components: [
           underTheBusObj,
@@ -264,62 +264,289 @@ const array = [
           busBodyObj,
           {
             title: 'main exhaust',
-            attributes: ['not more than 1/3 obstructed']
+            attributes: ['not more than 1/3 obstructed'],
           },
           {
             title: 'auxiliary exhaust',
-            attributes: ['not more than 1/3 obstructed']
+            attributes: ['not more than 1/3 obstructed'],
           },
           {
             title: 'differential',
-            attributes: [L]
+            attributes: [L],
           },
           {
             title: 'air bags',
-            attributes: [SI, 'securely mounted to control arm']
+            attributes: [SI, 'securely mounted to control arm'],
           },
           {
             title: 'control arm',
-            attributes: [SI]
-          }
-        ]
+            attributes: [SI],
+          },
+        ],
       },
       {
         title: 'passenger side',
         components: [
           {
             title: 'battery compartment',
-            attributes: [SI, 'terminals are not corroded']
+            attributes: [SI, 'terminals are not corroded'],
           },
           {
             title: 'battery switch',
-            attributes: ['turn on if necessary']
+            attributes: ['turn on if necessary'],
           },
           {
             title: 'passenger door',
-            attributes: [SI, 'glass is free of damage']
-          }
-        ]
+            attributes: [SI, 'glass is free of damage'],
+          },
+        ],
       },
     ],
   },
   {
     ID: 4,
-    title: 'passenger area',
-    subsections: [],
+    title: 'interior',
+    subsections: [
+      {
+        title: 'passenger area',
+        components: [
+          {
+            title: 'railing',
+            attributes: [SI],
+          },
+          {
+            title: 'seats',
+            attributes: [SI],
+          },
+          {
+            title: 'emergency exits',
+            attributes: ['latched', 'labeled', 'latches not sticking'],
+          },
+          {
+            title: 'tread',
+            attributes: ['not damaged'],
+          },
+          {
+            title: 'destination sign',
+            attributes: ['secured'],
+          },
+          {
+            title: 'foldable seats',
+            attributes: ['able to open and stow'],
+          },
+          {
+            title: 'wheelchair securements',
+            attributes: [
+              'enough securements for two wheelchairs (8)',
+              'free of damage',
+            ],
+          },
+        ],
+      },
+      {
+        title: 'driver cab',
+        components: [
+          {
+            title: 'fire extinguisher',
+            attributes: ['within expiration date', 'gauge in green'],
+          },
+          {
+            title: 'blood borne pathogen kit',
+            attributes: ['filled'],
+          },
+          {
+            title: 'first aid kit',
+            attributes: ['filled'],
+          },
+          {
+            title: 'emergency triangles',
+            attributes: ['three triangles', 'no damage'],
+          },
+          {
+            title: 'vehicle paperwork',
+            attributes: [],
+          },
+          {
+            title: 'electrical/fuse box',
+            attributes: ['spare fuses', 'no damage'],
+          },
+          {
+            title: 'driver seat',
+            attributes: [SI, 'not excessively loose'],
+          },
+        ],
+      },
+      {
+        title: 'in the driver seat',
+        components: [
+          {
+            title: 'seat belt',
+            attributes: [
+              'put on seatbelt if about to start the engine',
+              'secured to floor',
+              'no tears or damage',
+            ],
+          },
+          {
+            title: 'seat belt cutter',
+            attributes: [],
+          },
+          {
+            title: 'pedals',
+            attributes: [SI, 'noting underneath pedals'],
+          },
+          {
+            title: 'driver glass',
+            attributes: [WIND],
+          },
+          {
+            title: 'mirrors',
+            attributes: [WIND, 'adjusted for driver'],
+          },
+        ],
+      },
+      {
+        title: 'dashboard',
+        components: [
+          {
+            title: 'turn ignition: accessories',
+            attributes: [
+              'all dash lights cycle',
+              'wait for glow plug light to cycle',
+            ],
+          },
+          {
+            title: 'turn ignition: start engine',
+            attributes: [],
+          },
+          {
+            title: 'oil pressure',
+            attributes: [
+              'should come up to around 40 after start',
+              'normal pressure can range from 10-70 PSI',
+            ],
+          },
+          {
+            title: 'coolant temperature',
+            attributes: [
+              'will be low on startup',
+              'temps above 205F can be dangerous',
+            ],
+          },
+          {
+            title: 'air tank i & ii',
+            attributes: [
+              'are approaching similar pressure',
+              'are gradually rising with engine on',
+            ],
+          },
+          {
+            title: 'voltmeter',
+            attributes: [
+              'reading appropriately for battery (typically 1-3 volts above the battery size)',
+            ],
+          },
+          {
+            title: 'speedometer',
+            attributes: ['reading at zero when not moving'],
+          },
+          {
+            title: 'tachometer',
+            attributes: ['at appropriate rate', 'set high idle'],
+          },
+          {
+            title: 'transmission fluid temperature',
+            attributes: ['similar temperature to coolant'],
+          },
+          {
+            title: 'transmission fluid level',
+            attributes: ['check via dash'],
+          },
+        ],
+      },
+      {
+        title: 'steering column',
+        components: [
+          {
+            title: 'steering wheel adjuster',
+            attributes: [],
+          },
+          {
+            title: 'windshield wipers',
+            attributes: ['test that wipers spray and wipe adequately'],
+          },
+          {
+            title: 'high-beams/brights',
+            attributes: ['test that brights work from inside'],
+          },
+          {
+            title: 'steering wheel',
+            attributes: ['less than 10 degrees of play'],
+          },
+          {
+            title: 'horn',
+            attributes: ['honks'],
+          },
+        ],
+      },
+      {
+        title: 'miscellaneous dash',
+        components: [
+          {
+            title: 'mirror heater switch',
+            attributes: [],
+          },
+          {
+            title: 'headlight dial',
+            attributes: ['test that headlights work from inside'],
+          },
+          {
+            title: 'cab climate controls',
+            attributes: ['test that windshield defroster is warm'],
+          },
+          {
+            title: 'passenger climate controls',
+            attributes: [],
+          },
+          {
+            title: 'two-way radio',
+            attributes: [],
+          },
+          {
+            title: 'regen switches',
+            attributes: [],
+          },
+          {
+            title: 'retarder switch',
+            attributes: ['do not use in slippery conditions'],
+          },
+          {
+            title: 'off road switch',
+            attributes: ['for traction control'],
+          },
+          {
+            title: 'ramp enabler',
+            attributes: ['for ramp power'],
+          },
+          {
+            title: 'intercom switch and mic',
+            attributes: ['test that interior intercom works'],
+          },
+          {
+            title: 'destination sign',
+            attributes: ['test that sign works'],
+          },
+        ],
+      },
+    ],
   },
   {
     ID: 5,
-    title: 'driver cab',
-    subsections: [],
-  },
-  {
-    ID: 6,
     title: 'air brakes',
     subsections: [],
   },
   {
-    ID: 7,
+    ID: 6,
     title: 'lights',
     subsections: [],
   },
